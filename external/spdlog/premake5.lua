b4b29af504d92e5c159364307e1d0e023922898f
Chris <cdelicaat.me@gmail.com>
2025-07-28 22:57:27 +0200

    Removed unused GLM library added spdlog library for better loggin

---
+++ external/spdlog/premake5.lua
@@ -0,0 +1,19 @@
+﻿project "spdlog"
+    kind "None"  -- Header-only library
+    language "C++"
+    cppdialect "C++11"
+    staticruntime "on"
+
+    targetdir ("%{wks.location}/binaries/" .. outputdir .. "/Breakout")
+    objdir ("%{wks.location}/intermediate/" .. outputdir .. "/Breakout")
+    
+    files {
+        "include/spdlog/**.h",
+    }
+    
+    includedirs {
+        "include",
+    }
+
+    filter "action:vs*"
+        defines { "_CRT_SECURE_NO_WARNINGS" }
